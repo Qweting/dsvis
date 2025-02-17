718969c8fa7b62ead2102a5de567e855200e81d9
Josef.R <55584458+josefir98@users.noreply.github.com>
2025-02-17 17:30:06 +0100

    Added some messages and reset code for LinkedListAnim

--- src/basic/LinkedListAnim.js
+++ src/basic/LinkedListAnim.js
@@ -4,9 +4,43 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 DSVis.LinkedListAnim = class LinkedListAnim extends DSVis.Engine {
+    // Limit the size of the list to maintain readability
+    // maxListSize = 10;
+    
+    // Only used for hard-coded values
+    initialValues;
 
+    initialise(initialValues = null) {
+        this.initialValues = initialValues;
+        super.initialise();
+    }
+
+    async resetAlgorithm() {
+        await super.resetAlgorithm();
+        
+        if (this.initialValues) {
+            this.State.resetting = true;
+            await this.insert(...this.initialValues);
+            this.State.resetting = false;
+        }
+    }
 }
 
-DSVis.BinaryHeap.messages = {
-    
+DSVis.LinkedListAnim.messages = {
+    general: {
+        empty: "List is empty!",
+        full: "List is full!",
+        finished: "Finished",
+    },
+    find: {
+        start: (element) => `Searching for ${element}`,
+        found: (element) => `Found ${element}`,
+        notfound: (element) => `Did not find ${element}`,
+        look: (NextNode) => `Look into ${NextNode}`,
+    },
+    insert: {
+        element: (element) => `Insert element: ${element}`,
+    },
+    delete: {
+    },
 }
