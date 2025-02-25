477925985a807600e2d4079d24ba13cc9d7700ad
Dennis Holmström <dennis@holmstrom.nu>
2025-02-25 09:12:13 +0100

    Update webpack config to have sorcemap only in development

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const path = require("path");
 
-module.exports = {
-    mode: "production",
-    devtool: "eval-source-map",
-    entry: {
-        collections: "./src/collections.ts",
-        prioqueues: "./src/prioqueues.ts",
-        "avl-quiz": "./src/quizzes/AVL-quiz.ts",
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                include: [path.resolve(__dirname, "src")],
-            },
-        ],
-    },
-    output: {
-        publicPath: "public",
-        filename: "[name].js",
-        path: path.resolve(path.join(__dirname, "public", "js")),
-    },
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === "development";
+
+    return {
+        mode: isDevelopment ? "development" : "production",
+        devtool: isDevelopment ? "inline-source-map" : false,
+        entry: {
+            collections: "./src/collections.ts",
+            prioqueues: "./src/prioqueues.ts",
+            "avl-quiz": "./src/quizzes/AVL-quiz.ts",
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    include: [path.resolve(__dirname, "src")],
+                },
+            ],
+        },
+        output: {
+            publicPath: "public",
+            filename: "[name].js",
+            path: path.resolve(path.join(__dirname, "public", "js")),
+        },
+    };
 };
