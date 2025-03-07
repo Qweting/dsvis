4d6a678b797c5b85996bed738c49153694256ada
Josef.R <55584458+josefir98@users.noreply.github.com>
2025-03-07 12:17:38 +0100

    cleaned up linked-node

--- src/objects/basic-structure-objects/linked-node.ts
+++ src/objects/basic-structure-objects/linked-node.ts
@@ -3,94 +3,47 @@ import {
 } from "@svgdotjs/svg.js";
 
 export class LinkedNode extends G{
-    private _value: string | number;
-    private _rectWidth: number;
-    private _rectHeight: number; 
+    private value: string | number;
+    private rectWidth: number;
+    private rectHeight: number; 
     
-    private _elementRect: Rect;
-    private _textElement: Text; 
-    private _nextNodeRect: Rect;
+    private elementRect: Rect;
+    private textElement: Text; 
+    private nextNodeRect: Rect;
 
     constructor(value: string | number, objectSize: number) {
         super();
-        this._value = value;
-        this._rectWidth = 2*objectSize;
-        this._rectHeight = objectSize;
+        this.value = value;
+        this.rectWidth = 2*objectSize;
+        this.rectHeight = objectSize;
         
-        this._elementRect = this.rect(this._rectWidth, this._rectHeight); //initializing the rectangle for current node
-        this._nextNodeRect = this.rect(this._rectWidth / 2, this._rectHeight).move(this._rectWidth + 1, 0); //initializing the rectangle for next node
-        this._textElement = this.text(String(value)) //initializing the text element for current node (value)
-            .font({ size: this._rectHeight * 0.6 })
-            .center(this._elementRect.cx(), this._elementRect.cy());
+        this.elementRect = this.rect(this.rectWidth, this.rectHeight); //initializing the rectangle for current node
+        this.nextNodeRect = this.rect(this.rectWidth / 2, this.rectHeight).move(this.rectWidth + 1, 0); //initializing the rectangle for next node
+        this.textElement = this.text(String(value)) //initializing the text element for current node (value)
+            .font({ size: this.rectHeight * 0.6 })
+            .center(this.elementRect.cx(), this.elementRect.cy());
     }
 
-    get value(): string | number {
-        return this._value;
-    }
-
-    set value(value: string | number) {
-        this._value = value;
-    }
-
-    get rectWidth(): number {
-        return this._rectWidth;
-    }
-
-    set rectWidth(value: number) {
-        this._rectWidth = value;
-    }
-
-    get rectHeight(): number {
-        return this._rectHeight;
-    }
-
-    set rectHeight(value: number) {
-        this._rectHeight = value;
-    }
-
-    get elementRect(): Rect {
-        return this._elementRect;
-    }
-
-    set elementRect(value: Rect) {
-        this._elementRect = value;
-    }
-
-    get textElement(): Text {
-        return this._textElement;
-    }
-
-    //text element for the current node
-    set textElement(value: Text) {
-        this._textElement = value;
-    }
-
-    get nextElementRect(): Rect {
-        return this._nextNodeRect;
-    }
-
-    set nextElementRect(value: Rect) {
-        this._nextNodeRect = value;
-    }
+    
     
     // Get the center left coords of the node
     getLeft(): [number, number] {
-        return [this._elementRect.cx() - this._rectWidth / 2, this._elementRect.cy()];
+        return [this.elementRect.cx() - this.rectWidth / 2, this.elementRect.cy()];
     }
     
     // Get the center right coords of the node
     getRight(): [number, number] {
-        return [this._nextNodeRect.cx() + this._rectWidth / 4, this._elementRect.cy()];
+        return [this.nextNodeRect.cx() + this.rectWidth / 4, this.elementRect.cy()];
     }
 
     // Get the center top coords of the node
     getTop(): [number, number] {
-        return [this._elementRect.cx() + this._rectWidth / 2, this._elementRect.cy() - this._rectHeight / 2];
+        return [this.elementRect.cx() + this.rectWidth / 2, this.elementRect.cy() - this.rectHeight / 2];
     }
 
     // Get the center bottom coords of the node
     getBottom(): [number, number] {
-        return [this._elementRect.cx() + this._rectWidth / 2, this._elementRect.cy() + this._rectHeight / 2];
+        return [this.elementRect.cx() + this.rectWidth / 2, this.elementRect.cy() + this.rectHeight / 2];
     }
     
 }
