374bfd6e609ccca08edd20e99e393ef0db38726b
Dennis Holmström <dennis@holmstrom.nu>
2025-04-09 09:06:58 +0200

    Fix debugger like previous and initialize engine implementation

--- src/debugger.ts
+++ src/debugger.ts
@@ -3,8 +3,9 @@ export class Debugger {
 
     constructor() {
         // Get debug parameter from the URL query
-        const searchParams = new URLSearchParams(window.location.href);
-        const debugParam = searchParams.get("debug");
+        const debugParam = new URL(window.location.href).searchParams.get(
+            "debug"
+        );
 
         // Boolean conversion not prefect because only null or "" will return false aka any non empty string returns true
         this.enabled = Boolean(debugParam);
