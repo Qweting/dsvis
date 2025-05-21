6fa0644416534070c63ca1e06944d6c453683a78
Dennis Holmström <dennis@holmstrom.nu>
2025-05-21 10:29:22 +0200

    Fix mirroring issue and linked lists and format file

--- src/objects/basic-structure-objects/linked-node.ts
+++ src/objects/basic-structure-objects/linked-node.ts
@@ -1,4 +1,4 @@
-import { Text, G, Rect } from "@svgdotjs/svg.js";
+import { G, Rect, Text } from "@svgdotjs/svg.js";
 
 export class LinkedNode extends G {
     value: string | number;
@@ -14,7 +14,11 @@ export class LinkedNode extends G {
 
     private isMirrored: boolean = false;
 
-    constructor(value: string | number, nodeDimensions: [number, number], strokeWidth: number) {
+    constructor(
+        value: string | number,
+        nodeDimensions: [number, number],
+        strokeWidth: number
+    ) {
         super();
         this.value = value;
         this.nodeWidth = nodeDimensions[0];
@@ -22,30 +26,34 @@ export class LinkedNode extends G {
         this.elementRectWidth = this.nodeWidth * (3 / 4);
         this.nextNodeRectWidth = this.nodeWidth * (1 / 4);
 
-        this.elementRect = this.rect(this.elementRectWidth, this.nodeHeight).stroke({ width: strokeWidth }); //initializing the rectangle for current node
-        this.nextNodeRect = this.rect(
-            this.nextNodeRectWidth,
+        this.elementRect = this.rect(
+            this.elementRectWidth,
             this.nodeHeight
-        ).stroke({ width: strokeWidth }).move(this.elementRectWidth, 0); //initializing the rectangle for next node
+        ).stroke({ width: strokeWidth }); //initializing the rectangle for current node
+        this.nextNodeRect = this.rect(this.nextNodeRectWidth, this.nodeHeight)
+            .stroke({ width: strokeWidth })
+            .move(this.elementRectWidth, 0); //initializing the rectangle for next node
 
         this.textElement = this.text(String(value)) //initializing the text element for current node (value)
             .font({ size: this.nodeHeight * 0.6 })
-            .center(this.elementRect.cx() + 15, this.elementRect.cy()); // + 15 to center the text in the rectangle
-
+            .width(this.elementRectWidth)
+            .height(this.nodeHeight)
+            .amove(this.elementRectWidth / 2, 0)
+            .cy(this.nodeHeight / 2);
     }
 
     // mirrors the node so that elementRect and nextNodeRect are swapped
     mirror(flip: boolean): void {
-        if(flip !== this.isMirrored) {
-            if(!this.isMirrored) {
-                this.nextNodeRect.move(0, 0);
-                this.elementRect.move(this.nextNodeRectWidth, 0);
-                this.textElement.center(this.elementRect.cx(), this.elementRect.cy());
+        if (flip !== this.isMirrored) {
+            if (!this.isMirrored) {
+                this.nextNodeRect.dx(-this.elementRectWidth);
+                this.elementRect.dx(this.nextNodeRectWidth);
+                this.textElement.dx(this.nextNodeRectWidth);
                 this.isMirrored = true;
             } else {
-                this.elementRect.move(0, 0);
-                this.nextNodeRect.move(this.elementRectWidth, 0);
-                this.textElement.center(this.elementRect.cx() + 15, this.elementRect.cy());
+                this.nextNodeRect.dx(this.elementRectWidth);
+                this.elementRect.dx(-this.nextNodeRectWidth);
+                this.textElement.dx(-this.nextNodeRectWidth);
                 this.isMirrored = false;
             }
         }
@@ -65,7 +73,4 @@ export class LinkedNode extends G {
     getSize(): number {
         return this.nodeWidth;
     }
-
-
-
 }
